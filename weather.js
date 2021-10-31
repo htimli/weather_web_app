52963c5f03f4786587fa259248e9b055aa4e8be6
hassan timli <timlihassan@hotmail.fr>
2021-10-31 15:42:20 +0100

    get data after each click on the map

--- weather.js
+++ weather.js
@@ -3,44 +3,56 @@ const temperaturesMin = document.querySelectorAll('.tempMin');
 const temperaturesMax = document.querySelectorAll('.tempMax');
 const hoursInterval = 2;
 
+const data_container = document.querySelector('#data_container');
+const map = document.querySelector('#map');
 
 
+const frLat = 46.603;
+const frLng = 1.888 ;
 
-function getApiData(city_name) {
+const zoom = 6;
+const zoomMin = 1;
+const zoomMax = 20;
+
+
+function getApiData(urlPath) {
     
     function onResponse(response) {
-        return response.json();
-       
+        return response.json();      
     }
+
     function onError(error) {
         console.log("Error :  " + error);  
     }
+
     function onStreamProcessed(json) {
+
         console.log(json);
 
-        var keys = Object.keys(json)
-        
+        var keys = Object.keys(json)    
         if(keys[0] == "errors") {
             var error = json.errors[0];
+
             if(error .code == '11'){
+
                 //case where the City or coordinate not found
                 alert(error.text+"\n"+error.description);
             }  
         }else{
+
             setDomCurrentConditions(json); 
 
             setDomDailyConditionsData(json);     
 
             setDomHourlyConditionsData(json);
 
-            const data_container = document.querySelector('#data_container');
+            displayMap(false);
     
-            data_container.style.visibility  = 'visible';
         }
 
     }
 
-    fetch('https://prevision-meteo.ch/services/json/'+city_name)
+    fetch('https://prevision-meteo.ch/services/json/'+urlPath)
         .then(onResponse, onError)
         .then(onStreamProcessed);
 }
@@ -94,73 +106,72 @@ function setDomData(DomElement,data) {
     document.querySelector(DomElement).innerHTML = data;
 }
 
-function onClick(){
-    console.log("button clicked");
-    
+function onClickSearchButton(){
     city_name = document.querySelector("#form_button").value;
-
     getApiData(city_name);
 }
+
+function onClickMapButton(){
+    console.log('map button clicked ');   
+    displayMap(true);
+}
+
+
+function displayMap(showing){
+    let state ;
+    if(showing){
+        state= 'visible';
+    }
+    else{
+        state = 'hidden';
+    }
+    map.style.visibility = state;
+
+}
+// Initialize the map and set its view 
+function initMap(lat,lng,zoom) {
+    //2 46 4
+    return L.map('map').setView([lat, lng], zoom);
+}
+
+function addTileLayer(mymap) {
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+        attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+        minZoom: zoomMin,
+        maxZoom: zoomMax 
+    }).addTo(mymap);
+}
+                 
+
 
 
 function main() {
     const menu_button = document.querySelector('#menu_button');
-    menu_button.addEventListener('click',onClick);
+    const map_button = document.querySelector('#map_button');
 
+
+    menu_button.addEventListener('click',onClickSearchButton);
+
+    map_button.addEventListener('click',onClickMapButton);
+    
+    var mymap = initMap(frLat,frLng,zoom);
+    
+    addTileLayer(mymap);
+    
+    
+    mymap.on('click', function(e) {
+        console.log("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng);
+        let urlPath = `lat=${e.latlng.lat}lng=${e.latlng.lng}`;
+        getApiData(urlPath);
+    });
 }
-
-
-
-
-
-
 
 
 
 main();
 
 
-function onClickMapButton(){
-    console.log('map button clicked ');
-    const map = document.querySelector('#map');
-
-    map.style.display = "none";
-}
-
-const map_button = document.querySelector('#map_button');
-
-map_button.addEventListener('click',onClickMapButton);
 
 
-// initialize the map and set its view 
-//2 46 4
-var mymap = L.map('map').setView([46.603,1.888], 5);
 
-//add a tile layer
-
-L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-    // Il est toujours bien de laisser le lien vers la source des données
-    attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
-    minZoom: 1,
-    maxZoom: 20
-}).addTo(mymap);
-
-
-function onSuccess(response) {
-    return response.json();
-}
-function onError(error){
-    console.log('error'+error)
-}
-function onStreemProcessed(json){
-    console.log(json);
-}
-
-mymap.on('click', function(e) {
-    alert("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng)
-
-    fetch(`https://www.prevision-meteo.ch/services/json/lat=${e.latlng.lat}lng=${e.latlng.lng}`)
-    .then(onSuccess,onError)
-    .then(onStreemProcessed)
-});
-                                    
+                   
